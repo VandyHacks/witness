374a9b03afe342fa18dff2f72f5a0a73b53dd288
Samuel Lee <samlee514@Samuels-MacBook-Pro.local>
2021-09-03 10:37:26 -0500

    lint

--- pages/form.tsx
+++ pages/form.tsx
@@ -5,7 +5,7 @@ export default function FirstPost() {
 	return (
 		<Outline>
 			<h1>Judging form</h1>
-			<p>Dropdown at top specifying which team this is for. Judges' forms are associated with them by default.</p>
+			<p>Dropdown at top specifying which team this is for. Judging forms are associated with them by default.</p>
 			<p>Should be able to save / submit. Either way it can be retroactively edited.</p>
 		</Outline>
 	);
