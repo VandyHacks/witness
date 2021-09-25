5aca88601478dd57a92a7b07cd12e4968e34bf60
Samuel Lee <samlee514@Samuels-MacBook-Pro-2.local>
2021-09-25 16:34:43 -0500

    little green checkmark

--- components/teamSelect.tsx
+++ components/teamSelect.tsx
@@ -1,8 +1,18 @@
-import { Select, Space } from 'antd';
+import { Select, Space, Row } from 'antd';
+import { CheckCircleOutlined } from '@ant-design/icons'
 const { Option, OptGroup } = Select;
 
 interface TeamSelectProps {
 	handleChange(id: string): void;
+}
+
+function showCompletedInDropdown(value: string) {
+	return (
+		<Row justify="space-between" align="middle">
+			{value}
+			<CheckCircleOutlined style={{color: "green"}}/>
+		</Row>
+	)
 }
 export default function TeamSelect(props: TeamSelectProps) {
 	const { handleChange } = props;
@@ -11,7 +21,7 @@ export default function TeamSelect(props: TeamSelectProps) {
 			<strong>Select a Team</strong>
 			<Select defaultValue="lucy" style={{ width: 200 }} onChange={handleChange}>
 				<OptGroup label="My Teams">
-					<Option value="jack">Jack</Option>
+					<Option value="jack">{showCompletedInDropdown("Jack")}</Option>
 					<Option value="lucy">Lucy</Option>
 				</OptGroup>
 				<OptGroup label="All Teams">
