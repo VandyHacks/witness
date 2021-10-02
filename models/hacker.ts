30bb04ef18e5493846c6f964ad0a277078568ff1
Aadi Bajpai <hello@aadibajpai.com>
2021-10-02 03:02:32 -0500

    add team schema and run prettier

--- models/hacker.ts
+++ models/hacker.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 // dummy schema for use with Vaken's existing db
-const HackerSchema = new Schema({}, { strict: false });
+export const HackerSchema = new Schema({}, { strict: false });
 
 // prevent recompilation of model if it already exists
 export default mongoose.models.Hacker || mongoose.model('Hacker', HackerSchema, 'Hackers');
--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,18 +1,18 @@
-import dbConnect from "../../middleware/database";
-import Hacker from "../../models/hacker";
+import dbConnect from '../../middleware/database';
+import Hacker from '../../models/hacker';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 interface Data {
-    success: string,
-    data: JSON,
-};
+	success: string;
+	data: JSON;
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    const { method } = req;
-    await dbConnect();
+	const { method } = req;
+	await dbConnect();
 
-    const hacker = await Hacker.find({firstName: "Samuel"});
-    console.log(hacker);
+	const hacker = await Hacker.find({ firstName: 'Samuel' });
+	console.log(hacker);
 
-	res.status(200).json({success: "true", data: hacker as unknown as JSON});
+	res.status(200).json({ success: 'true', data: hacker as unknown as JSON });
 }
