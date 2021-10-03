f7120d5c5ba08b27960c35d7f78b70d48b4530c0
Aadi Bajpai <hello@aadibajpai.com>
2021-10-03 00:01:27 -0500

    add judging schema and use vaken db for Hackers

--- models/team.ts
+++ models/team.ts
@@ -3,26 +3,35 @@ import { HackerSchema } from './hacker';
 
 const Schema = mongoose.Schema;
 
-const TeamSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	members: {
-		type: [HackerSchema],
-		required: true,
-		validate: {
-			// check team size
-			validator: (arr: Array<typeof HackerSchema>) => arr.length <= 4,
-			message: 'Max team size is 4 members.',
+const TeamSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
 		},
+		joinCode: {
+			type: String,
+			required: true,
+		},
+		devpost: {
+			type: String,
+			required: true,
+		},
+		members: {
+			type: [HackerSchema],
+			required: true,
+			validate: {
+				// check team size
+				validator: (arr: Array<typeof HackerSchema>) => arr.length <= 4,
+				message: 'Max team size is 4 members.',
+			},
+		},
+		judging: { type: Schema.Types.ObjectId, ref: 'Judgement' },
 	},
-	devpost: {
-		type: String,
-		required: true,
-	},
-	// add more stuff like judges
-});
+	{
+		timestamps: true,
+	}
+);
 
 // prevent recompilation of model if it already exists
 export default mongoose.models.Team || mongoose.model('Team', TeamSchema);
