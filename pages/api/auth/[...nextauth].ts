46ef00b976532d86a007ffac0888e702b24b021c
Samuel Lee <sam@samlee.dev>
2021-10-05 01:05:41 -0500

    some stuff

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import dbConnect from '../../../middleware/database';
+import Hacker from '../../../models/hacker';
 
 export default NextAuth({
 	// Configure one or more authentication providers
@@ -15,6 +17,28 @@ export default NextAuth({
 		}),
 		// ...add more providers here
 	],
+	session: {
+		jwt: true,
+	},
+	// felix@yahoo.com
+	callbacks: {
+		async jwt(token, user) {
+			await dbConnect();
+			if (user) {
+				// user is only defined on first sign in
+				const hacker = await Hacker.findOne({ email: user.email });
+				token.vaken = hacker;
+				console.log('User:', user);
+			}
+
+			return token;
+		},
+		async session(session, token) {
+			if (!session.vaken) session.vaken = token.vaken;
+			console.log('session:', session);
+			return session;
+		},
+	},
 
 	// A database is optional, but required to persist accounts in a database
 	database: process.env.DATABASE_URL,
--- pages/api/test.ts
+++ pages/api/test.ts
@@ -2,6 +2,7 @@ import dbConnect from '../../middleware/database';
 import Hacker from '../../models/hacker';
 import Team from '../../models/team';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/client';
 
 interface Data {
 	success: string;
@@ -11,10 +12,12 @@ interface Data {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 	const { method } = req;
+	const session = await getSession({ req });
+	const hacker = session?.hacker;
+	if (!hacker) {
+		res.status(404);
+	}
 	await dbConnect();
-
-	const hacker = await Hacker.find({ firstName: 'Felix' });
-	console.log(hacker);
 
 	const team = {
 		name: 'test team ez',
