7d665fe7c82fd1e68e4382d3df8ff39432a2c9bc
Samuel Lee <sam@samlee.dev>
2021-10-06 05:49:38 -0500

    remove the offender

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import { signIn, signOut, useSession } from 'next-auth/client';
-import Outline from '../components/outline';
+// import { signIn, signOut, useSession } from 'next-auth/client';
+// import Outline from '../components/outline';
 
-export default function Page() {
-	const [session, loading] = useSession();
+// export default function Page() {
+// 	const [session, loading] = useSession();
 
-	return (
-		<Outline>
-			{!session && (
-				<>
-					Not signed in <br />
-					<button onClick={() => signIn()}>Sign in</button>
-				</>
-			)}
-			{session && (
-				<>
-					Signed in as {session.user?.email} <br />
-					<button onClick={() => signOut()}>Sign out</button>
-				</>
-			)}
-		</Outline>
-	);
-}
+// 	return (
+// 		<Outline>
+// 			{!session && (
+// 				<>
+// 					Not signed in <br />
+// 					<button onClick={() => signIn()}>Sign in</button>
+// 				</>
+// 			)}
+// 			{session && (
+// 				<>
+// 					Signed in as {session.user?.email} <br />
+// 					<button onClick={() => signOut()}>Sign out</button>
+// 				</>
+// 			)}
+// 		</Outline>
+// 	);
+// }
