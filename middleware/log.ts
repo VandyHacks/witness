37a5327e6ba1c40c352c87a9f63eb73e9a892458
nkalupahana <avishkank@gmail.com>
2021-10-08 16:32:30 -0500

    Switched to env variable

--- middleware/log.ts
+++ middleware/log.ts
@@ -1,7 +1,8 @@
 import fetch from "node-fetch";
+const { SLACK_LOGGING_WEBHOOK } = process.env;
 
 export default async function log(id: any, data: any) {
-    await fetch("https://hooks.slack.com/workflows/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", {
+    await fetch(SLACK_LOGGING_WEBHOOK as string, {
         method: "POST",
         body: JSON.stringify({
             id,
