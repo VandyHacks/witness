7fe96abbbe060d0bd0dc4f51963ea989d6801652
Aadi Bajpai <hello@aadibajpai.com>
2021-10-07 21:56:54 -0500

    implement team routes

--- pages/api/team-management.ts
+++ pages/api/team-management.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { customAlphabet } from 'nanoid';
-import Scores from '../../models/scores';
 import Team from '../../models/team';
 import dbConnect from '../../middleware/database';
 import { getSession } from 'next-auth/client';
 import { TeamProfile } from '../team';
+import User from '../../models/user';
 
 const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz', 7);
 
@@ -18,10 +18,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	const session = await getSession({ req });
 	if (session?.userType !== 'HACKER') return res.status(403).send('Forbidden');
 	// TODO: add uniqueness validation try catch
+	await dbConnect();
+	const hacker = await User.findById(session.userID);
 	switch (req.method) {
 		case 'GET':
-			const team = await Team.findOne({ members: { $in: [session.userID] } });
-			return res.status(200).send(team);
+			const team = await Team.findOne({ "members.id": session.userID });
+			console.log("TEAM:", team)
+			return res.status(team ? 200 : 409).send(team);
 		case 'POST': {
 			const { joinCode, teamName, devpost } = req.body;
 			// join team
@@ -29,7 +32,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 				const team = await Team.findOne({ joinCode });
 				if (!team) return res.status(404).send('Team not found');
 
-				team.members.push(session.userID);
+				team.members.push(hacker);
 				await team.save();
 				return res.status(201).send(team);
 			} else if (teamName) {
@@ -38,31 +41,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 					name: teamName,
 					joinCode: nanoid(),
 					devpost,
-					members: [session.userID],
+					members: [hacker],
 				};
+				console.log(teamObj);
 				const team = new Team(teamObj);
 				await team.save();
-				res.status(201).send(team);
+				return res.status(201).send(team);
+			} else {
+				return res.status(400).send('Either joinCode or teamName required.');
 			}
-			return res.status(400).send('Either joinCode or teamName required.');
 		}
 		case 'PATCH': {
-			const team = await Team.findOne({ members: { $in: [session.userID] } });
+			console.log("owo in patch");
+			const team = await Team.findOne({ "members.id": session.userID });
 			if (!team) return res.status(404).send('Team not found');
 
 			const { teamName, devpost } = req.body;
 			if (devpost) team.devpost = devpost;
-			if (teamName) team.teamName = teamName;
+			if (teamName) team.name = teamName;
 
 			await team.save();
+			console.log("new team", team);
 			return res.status(200).send(team);
 		}
 
 		case 'DELETE': {
 			const { userID } = session;
-			let team = await Team.findOne({ members: { $in: [userID] } });
+			let team = await Team.findOne({ "members.id": session.userID });
 			if (!team) return res.status(404).send('Team not found');
-			team.members = team.members.filter((id: unknown) => id !== userID);
+			team.members = team.members.filter((member: { id: string; }) => member.id !== userID);
+			if (!team.members.length) {
+				await Team.deleteOne({ "members.id": session.userID });
+				return res.status(200).send()
+			}
 			await team.save();
 			return res.status(200).send(team);
 		}
