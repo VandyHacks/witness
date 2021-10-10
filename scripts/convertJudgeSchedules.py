b43e9de03226599f23144c9df65ce4d7c2f905e0
Akash <Ace314159@users.noreply.github.com>
2021-10-10 01:45:32 -0500

    Fix when teams aren't divisible (#47)
    * Fix when teams aren't divisible
    
    * Fix more

--- scripts/convertJudgeSchedules.py
+++ scripts/convertJudgeSchedules.py
@@ -44,12 +44,21 @@ with open(f"{dir_path}/output.csv", "w", newline="") as f:
 
     team_num = 0
     time = START_TIME
+    done = False
     for session_num, session in enumerate(sessions):
         if session_num == len(sessions) // 2:
             time += TIME_DELTA_10  # Add to account for the break
         for i, triplet in enumerate(session):
             judge_names = [JUDGES[i] for i in triplet]
+            if team_num >= len(TEAMS):
+                done = True
+                break
             line = [datetime.strftime(time, TIME_FMT) + TZ_EXTRA, f"vhl.ink/room-{i+1}", *judge_names, TEAMS[team_num]]
             writer.writerow(line)
             team_num += 1
         time += TIME_DELTA_10
+        if done:
+            break
+
+if team_num < len(TEAMS):
+    print("Not all teams being judged")
