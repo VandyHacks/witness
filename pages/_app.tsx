d311a8540a11275ba5c0e54fd6bf7494bc9981b2
Baibhav Vatsa <baibhavvatsa@gmail.com>
2021-10-09 20:39:08 -0500

    add favicon (#34)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import { Provider } from 'next-auth/client';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
+		<>
+		<Head>
+			<link rel="icon" type="image/x-icon" href="favicon.ico" />
+		</Head>
 		<Provider session={pageProps.session}>
 			<Component {...pageProps} />
 		</Provider>
+		</>
 	);
 }
 export default MyApp;
