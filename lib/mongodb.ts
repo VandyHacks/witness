02463f7f848dc575b7f4bb3b868d91ba097c98ef
Samuel Lee <sam@samlee.dev>
2021-11-14 11:41:05 -0600

    Add Mongo driver

---
+++ lib/mongodb.ts
@@ -0,0 +1,34 @@
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.DATABASE_URL;
+const options = {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+};
+
+let client;
+let clientPromise;
+
+if (!process.env.DATABASE_URL) {
+	// TODO: make this .env.local to use Next properly (see: https://nextjs.org/docs/basic-features/environment-variables)
+	throw new Error('Please add your Mongo URI to .env');
+}
+
+if (process.env.NODE_ENV === 'development') {
+	// In development mode, use a (global as any) variable so that the value
+	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+	if (!(global as any)._mongoClientPromise) {
+		client = new MongoClient(uri!, options);
+		(global as any)._mongoClientPromise = client.connect();
+	}
+	clientPromise = (global as any)._mongoClientPromise;
+} else {
+	// In production mode, it's best to not use a (global as any) variable.
+	client = new MongoClient(uri!, options);
+	clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise as Promise<MongoClient>;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,65 +1,72 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import { TypeORMLegacyAdapter } from '@next-auth/typeorm-legacy-adapter';
+// import { TypeORMLegacyAdapter } from '@next-auth/typeorm-legacy-adapter';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../lib/mongodb';
 import dbConnect from '../../../middleware/database';
 import vakenLogin from '../../../models/vakenLogin';
 import User from '../../../models/user';
 
-export default NextAuth({
-	providers: [
-		GitHubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_ID,
-			clientSecret: process.env.GOOGLE_SECRET,
-		}),
-	],
-	session: {
-		jwt: true,
-	},
-	callbacks: {
-		async signIn({ user, account, profile }) {
-			if (account.provider == 'github') {
-				const res = await fetch('https://api.github.com/user/emails', {
-					headers: { Authorization: `token ${account.accessToken}` },
-				});
-				const emails = await res.json();
-				if (emails?.length > 0) {
-					user.email = emails.sort((a: any, b: any) => b.primary - a.primary)[0].email;
+export default async function auth(req: any, res: any) {
+	await NextAuth(req, res, {
+		providers: [
+			GitHubProvider({
+				clientId: process.env.GITHUB_ID || '',
+				clientSecret: process.env.GITHUB_SECRET || '',
+			}),
+			GoogleProvider({
+				clientId: process.env.GOOGLE_ID || '',
+				clientSecret: process.env.GOOGLE_SECRET || '',
+			}),
+		],
+		session: {
+			jwt: true,
+		},
+		callbacks: {
+			// async signIn({ user, account, profile }) {
+			// 	if (account.provider == 'github') {
+			// 		const res = await fetch('https://api.github.com/user/emails', {
+			// 			headers: { Authorization: `token ${account.accessToken}` },
+			// 		});
+			// 		const emails = await res.json();
+			// 		if (emails?.length > 0) {
+			// 			user.email = emails.sort((a: any, b: any) => b.primary - a.primary)[0].email;
+			// 		}
+			// 	}
+
+			// 	return true;
+			// },
+			async jwt({ token, user, account }) {
+				await dbConnect();
+				if (user) {
+					// user is only defined on first sign in
+					const login = await User.findOne({ email: user.email });
+
+					// read usertype from vaken db
+					if (!login.userType) {
+						const vakenUser = await vakenLogin.findOne({ email: user.email }).lean();
+						if (vakenUser?.userType) login.userType = vakenUser.userType;
+						await login.save();
+					}
+
+					token.userType = login.userType;
 				}
-			}
-
-			return true;
-		},
-		async jwt({ token, user, account }) {
-			await dbConnect();
-			if (user) {
-				// user is only defined on first sign in
-				const login = await User.findOne({ email: user.email });
-
-				// read usertype from vaken db
-				if (!login.userType) {
-					const vakenUser = await vakenLogin.findOne({ email: user.email }).lean();
-					if (vakenUser?.userType) login.userType = vakenUser.userType;
-					await login.save();
+				return token;
+			},
+			async session({ session, token }) {
+				if (!session.userType || !session.userID) {
+					session.userType = token.userType;
+					session.userID = token.sub;
 				}
-
-				token.userType = login.userType;
-			}
-			return token;
+				return session;
+			},
 		},
-		async session({ session, token }) {
-			if (!session.userType || !session.userID) {
-				session.userType = token.userType;
-				session.userID = token.sub;
-			}
-			return session;
-		},
-	},
 
-	// A database is optional, but required to persist accounts in a database
-	adapter: TypeORMLegacyAdapter(process.env.DATABASE_URL as string),
-});
+		// A database is optional, but required to persist accounts in a database
+		// adapter: TypeORMLegacyAdapter(process.env.DATABASE_URL as string),
+		adapter: MongoDBAdapter({
+			db: (await clientPromise).db('witness'),
+		}),
+	});
+}
