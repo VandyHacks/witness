ad1788b673542933747a42625bfb1ccd192175cc
Aadi Bajpai <hello@aadibajpai.com>
2022-06-27 22:24:20 -0400

    show changed and fix text

--- components/manageRoleForm.tsx
+++ components/manageRoleForm.tsx
@@ -1,60 +1,73 @@
 import { Form, Button, Select } from 'antd';
 import Text from 'antd/lib/typography/Text';
+import { useState } from 'react';
 
 const { Option } = Select;
 
 export interface ManageFormFields {
-  _id: string;
-  name: string;
-  userType: string;
-  email: string;
+	_id: string;
+	name: string;
+	userType: string;
+	email: string;
 }
 
 export interface ManageFormProps {
-  formData: ManageFormFields[];
-  onSubmit: (value: ManageFormFields) => Promise<void>;
+	formData: ManageFormFields[];
+	onSubmit: (value: ManageFormFields) => Promise<void>;
 }
 
 export default function ManageRoleForm(props: ManageFormProps) {
-  const { onSubmit } = props;
-  const layout = {
-    labelCol: { span: 5 },
-    wrapperCol: { span: 3 },
-    labelAlign: 'left',
-  };
+	const { onSubmit } = props;
+	const layout = {
+		labelCol: { span: 5 },
+		wrapperCol: { span: 3 },
+		labelAlign: 'left',
+	};
 
-  // TODO: probably add search
-  const [form] = Form.useForm();
+	const [modified, setModified] = useState<string[]>([]);
 
-  return (
-    <>
-      <Form {...layout} labelAlign="left" form={form} onFinish={onSubmit}>
-        {props.formData.map(config => (
-          <Form.Item
-            name={config._id}
-            // display name and email
-            label={
-              <Text>
-                {config.name}
-                <br />
-                <Text type="secondary">{config.email}</Text>
-              </Text>
-            }
-            colon={false}
-            key={config._id}
-            initialValue={config.userType}
-          >
-            <Select placeholder="Select Role">
-              <Option value="HACKER">Hacker</Option>
-              <Option value="JUDGE">Judge</Option>
-              <Option value="ORGANIZER">Organizer</Option>
-            </Select>
-          </Form.Item>
-        ))}
-        <Button type="primary" htmlType="submit" className="ant-col-offset-12">
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
+	// TODO: probably add search
+	const [form] = Form.useForm();
+
+	return (
+		<Form {...layout} labelAlign="left" form={form} onFinish={onSubmit}>
+			{props.formData.map(config => (
+				<Form.Item
+					name={config._id}
+					// display name and email
+					label={
+						<Text style={{ height: '50px' }}>
+							{config.name}
+							<br />
+							<Text type="secondary">{config.email}</Text>
+						</Text>
+					}
+					colon={false}
+					key={config._id}
+					initialValue={config.userType}
+				>
+					<Select
+						placeholder="Select Role"
+						status={modified.includes(config._id) ? 'warning' : ''}
+						// not really a warning just good visually
+						// make box glow if role has been changed
+						onSelect={(role: string) => {
+							if (role !== props.formData.find(user => user._id === config._id)?.userType) {
+								setModified([...modified, config._id]);
+							} else {
+								setModified([...modified.filter(user => user !== config._id)]);
+							}
+						}}
+					>
+						<Option value="HACKER">Hacker</Option>
+						<Option value="JUDGE">Judge</Option>
+						<Option value="ORGANIZER">Organizer</Option>
+					</Select>
+				</Form.Item>
+			))}
+			<Button type="primary" htmlType="submit" className="ant-col-offset-12">
+				Submit
+			</Button>
+		</Form>
+	);
 }
