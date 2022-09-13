341d4c1083167b7dce9ea00aae07616ba19afd34
zineanteoh <zineanteoh@users.noreply.github.com>
2022-09-13 03:28:39 +0000

    Run Prettier

--- pages/HackerDash.tsx
+++ pages/HackerDash.tsx
@@ -87,7 +87,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 										display: 'flex',
 										flexDirection: 'column',
 										alignItems: 'center',
-									}}>
+									}}
+								>
 									<div>Signed in as {userEmail}</div>
 									<div>Not you? </div>
 									<button onClick={() => signOut()}>Sign out</button>
@@ -95,13 +96,15 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 								<Form.Item
 									label="First Name"
 									name="firstName"
-									rules={[{ required: true, message: 'Please input your first name!' }]}>
+									rules={[{ required: true, message: 'Please input your first name!' }]}
+								>
 									<Input className={styles.Input} />
 								</Form.Item>
 								<Form.Item
 									label="Last Name"
 									name="lastName"
-									rules={[{ required: true, message: 'Please input your last name!' }]}>
+									rules={[{ required: true, message: 'Please input your last name!' }]}
+								>
 									<Input className={styles.Input} />
 								</Form.Item>
 								<Form.Item label="Preferred Name" name="preferredName">
@@ -110,13 +113,15 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 								<Form.Item
 									label="Phone Number"
 									name="phoneNumber"
-									rules={[{ required: true, message: 'Please input your phone number!' }]}>
+									rules={[{ required: true, message: 'Please input your phone number!' }]}
+								>
 									<Input className={styles.Input} />
 								</Form.Item>
 								<Form.Item
 									label="Gender"
 									name="gender"
-									rules={[{ required: true, message: 'Please select an option!' }]}>
+									rules={[{ required: true, message: 'Please select an option!' }]}
+								>
 									<Radio.Group>
 										<Radio.Button value="female">Female</Radio.Button>
 										<Radio.Button value="male">Male</Radio.Button>
@@ -130,25 +135,29 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 								<Form.Item
 									name="dateOfBirth"
 									label="Date of Birth"
-									rules={[{ required: true, message: 'Please select your date of birth!' }]}>
+									rules={[{ required: true, message: 'Please select your date of birth!' }]}
+								>
 									<DatePicker />
 								</Form.Item>
 								<Form.Item
 									label="School"
 									name="school"
-									rules={[{ required: true, message: 'Please input your school!' }]}>
+									rules={[{ required: true, message: 'Please input your school!' }]}
+								>
 									<Input className={styles.Input} />
 								</Form.Item>
 								<Form.Item
 									label="Major"
 									name="major"
-									rules={[{ required: true, message: 'Please input your major!' }]}>
+									rules={[{ required: true, message: 'Please input your major!' }]}
+								>
 									<Input className={styles.Input} />
 								</Form.Item>
 								<Form.Item
 									label="Graduation Year"
 									name="graduationYear"
-									rules={[{ required: true, message: 'Please select your graduation year!' }]}>
+									rules={[{ required: true, message: 'Please select your graduation year!' }]}
+								>
 									<Radio.Group>
 										<Radio.Button value="2023">2023</Radio.Button>
 										<Radio.Button value="2024">2024</Radio.Button>
@@ -160,19 +169,22 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 								<Form.Item
 									name="race"
 									label="Race"
-									rules={[{ required: true, message: 'Please select at least one option!' }]}>
+									rules={[{ required: true, message: 'Please select at least one option!' }]}
+								>
 									<Checkbox.Group options={race} />
 								</Form.Item>
 								<Form.Item
 									name="motivation"
 									label="What do you hope to gain from VandyHacks?"
-									rules={[{ required: true, message: 'Please select at least one option!' }]}>
+									rules={[{ required: true, message: 'Please select at least one option!' }]}
+								>
 									<Checkbox.Group options={motivation} />
 								</Form.Item>
 								<Form.Item
 									label="Will you be attending the hackathon in-person?"
 									name="attendingInPerson"
-									rules={[{ required: true, message: 'Please select an answer!' }]}>
+									rules={[{ required: true, message: 'Please select an answer!' }]}
+								>
 									<Radio.Group>
 										<Radio.Button value="yes">Yes</Radio.Button>
 										<Radio.Button value="no">No</Radio.Button>
@@ -181,7 +193,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 								<Form.Item
 									label="Would you like to be contacted about volunteering at the event?"
 									name="volunteer"
-									rules={[{ required: true, message: 'Please select an answer!' }]}>
+									rules={[{ required: true, message: 'Please select an answer!' }]}
+								>
 									<Radio.Group>
 										<Radio.Button value="yes">Yes</Radio.Button>
 										<Radio.Button value="no">No</Radio.Button>
@@ -191,20 +204,23 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 									label={'Résumé (will be shared with sponsors)'}
 									rules={[{ required: true, message: 'Please upload your résumé!' }]}
 									name="resume"
-									valuePropName="resume">
+									valuePropName="resume"
+								>
 									<Upload
 										name="resume"
 										action="/api/upload-resume"
 										listType="picture"
 										accept=".pdf"
-										maxCount={1}>
+										maxCount={1}
+									>
 										<Button icon={<UploadOutlined />}>Click to Upload Résumé</Button>
 									</Upload>
 								</Form.Item>
 								<Form.Item
 									label="Address Line 1"
 									name="address1"
-									rules={[{ required: true, message: 'Please input your address!' }]}>
+									rules={[{ required: true, message: 'Please input your address!' }]}
+								>
 									<Input className={styles.Input} />
 								</Form.Item>
 								<Form.Item label="Address Line 2" name="address2">
@@ -215,7 +231,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 										<Form.Item
 											label="City"
 											name={'city'}
-											rules={[{ required: true, message: 'Please input your city!' }]}>
+											rules={[{ required: true, message: 'Please input your city!' }]}
+										>
 											<Input className={styles.Input + ' ' + styles.InputCity} />
 										</Form.Item>
 									</Col>
@@ -223,7 +240,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 										<Form.Item
 											label="State"
 											name={'state'}
-											rules={[{ required: true, message: 'Please input your state!' }]}>
+											rules={[{ required: true, message: 'Please input your state!' }]}
+										>
 											<Input className={styles.Input + ' ' + styles.InputState} />
 										</Form.Item>
 									</Col>
@@ -231,7 +249,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 										<Form.Item
 											label="ZIP Code"
 											name={'zip'}
-											rules={[{ required: true, message: 'Please input your zip code!' }]}>
+											rules={[{ required: true, message: 'Please input your zip code!' }]}
+										>
 											<Input className={styles.Input + ' ' + styles.InputZip} />
 										</Form.Item>
 									</Col>
@@ -239,7 +258,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 								<Form.Item
 									label="Shirt Size"
 									name="shirtSize"
-									rules={[{ required: true, message: 'Please select your shirt size!' }]}>
+									rules={[{ required: true, message: 'Please select your shirt size!' }]}
+								>
 									<Radio.Group>
 										<Radio.Button value="XS">XS</Radio.Button>
 										<Radio.Button value="S">S</Radio.Button>
@@ -260,13 +280,15 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 													? Promise.resolve()
 													: Promise.reject(new Error('Please read and agree to submit.')),
 										},
-									]}>
+									]}
+								>
 									<Checkbox>
 										I have read and agree to the{' '}
 										<a
 											target="_blank"
 											rel="noopener noreferrer"
-											href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf">
+											href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+										>
 											MLH Code of Conduct
 										</a>
 										.
@@ -282,7 +304,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 													? Promise.resolve()
 													: Promise.reject(new Error('Please read and agree to submit.')),
 										},
-									]}>
+									]}
+								>
 									<Checkbox>
 										I authorize you to share my application/registration information for event
 										administration, ranking, MLH administration, pre- and post-event informational
@@ -291,7 +314,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 										<a
 											target="_blank"
 											rel="noopener noreferrer"
-											href="https://github.com/MLH/mlh-policies/tree/master/prize-terms-and-conditions">
+											href="https://github.com/MLH/mlh-policies/tree/master/prize-terms-and-conditions"
+										>
 											MLH Contest Terms and Conditions
 										</a>{' '}
 										and the{' '}
@@ -321,7 +345,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 										href="https://www.instagram.com/vandyhacks"
 										target="_blank"
 										rel="noreferrer"
-										style={{ color: '#0000EE' }}>
+										style={{ color: '#0000EE' }}
+									>
 										Instagram
 									</a>{' '}
 									to stay updated on our news and announcements!
@@ -333,7 +358,8 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 											display: 'flex',
 											flexDirection: 'column',
 											alignItems: 'center',
-										}}>
+										}}
+									>
 										<div>Signed in as {userEmail}</div>
 										<button onClick={() => signOut()}>Sign out</button>
 									</div>
