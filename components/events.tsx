2b0446acd11ac08e3b92be378d3e192cfdf1fae6
Akash Munagala <akash.munagala@gmail.com>
2022-09-24 02:50:56 -0500

    Format using prettier

--- components/events.tsx
+++ components/events.tsx
@@ -1,64 +1,67 @@
-import { ConsoleSqlOutlined } from "@ant-design/icons";
-import { Button, Modal, Table } from "antd";
-import { useEffect, useState } from "react";
-import { EventData } from "../types/database";
+import { ConsoleSqlOutlined } from '@ant-design/icons';
+import { Button, Modal, Table } from 'antd';
+import { useEffect, useState } from 'react';
+import { EventData } from '../types/database';
 
 interface EventDisplay extends EventData {
 	setCurEvent: (open: String) => void;
 }
 
 const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-    },
-    {
-        title: 'Check In',
-        render: (_: any, record: EventDisplay) => {
-            return <Button onClick={() => record.setCurEvent(record.name)}>Check In</Button>
-        },
-    }
+	{
+		title: 'Name',
+		dataIndex: 'name',
+	},
+	{
+		title: 'Check In',
+		render: (_: any, record: EventDisplay) => {
+			return <Button onClick={() => record.setCurEvent(record.name)}>Check In</Button>;
+		},
+	},
 ];
 
 const Events = () => {
-    const [curEvent, setCurEvent] = useState(undefined);
-    const [events, setEvents] = useState<EventDisplay[]>([]);
-    const [loading, setLoading] = useState(false);
+	const [curEvent, setCurEvent] = useState(undefined);
+	const [events, setEvents] = useState<EventDisplay[]>([]);
+	const [loading, setLoading] = useState(false);
 
-    const getData = () => {
-        return fetch("/api/events")
-            .then((res) => res.json())
-            .then((data) => {
-                setEvents(data.map((obj: EventData) => {
-                    return {
-                        key: obj._id,
-                        setCurEvent,
-                        ...obj
-                    }
-                }));
-            });
-    }
+	const getData = () => {
+		return fetch('/api/events')
+			.then(res => res.json())
+			.then(data => {
+				setEvents(
+					data.map((obj: EventData) => {
+						return {
+							key: obj._id,
+							setCurEvent,
+							...obj,
+						};
+					})
+				);
+			});
+	};
 
-    const syncCalendar = async () => {
-        setLoading(true);
-        await fetch("/api/sync-calendar");
-        getData().then(() => setLoading(false));
-    };
+	const syncCalendar = async () => {
+		setLoading(true);
+		await fetch('/api/sync-calendar');
+		getData().then(() => setLoading(false));
+	};
 
-    useEffect(() => {
-        getData();
-    }, []);
+	useEffect(() => {
+		getData();
+	}, []);
 
-    return (
-        <>
-            <Button loading={loading} onClick={syncCalendar}>Sync Calendar Events</Button>
-            <br /><br />
-            <Table sticky bordered dataSource={events} columns={columns} />
-            <Modal title={`${curEvent} Check-in`} open={curEvent}>
-                
-            </Modal>
-        </>
-    );
+	return (
+		<>
+			<Button loading={loading} onClick={syncCalendar}>
+				Sync Calendar Events
+			</Button>
+			<br />
+			<br />
+			<Table sticky bordered dataSource={events} columns={columns} />
+			<Modal title={`${curEvent} Check-in`} open={curEvent}></Modal>
+		</>
+	);
 };
 
 export default Events;
