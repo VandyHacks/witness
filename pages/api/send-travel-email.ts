e45e93fa1f3873bbb5fab29b54f2a681581c4e19
PatelSneh21 <PatelSneh21@users.noreply.github.com>
2022-09-26 23:04:03 +0000

    Run Prettier

--- pages/api/send-travel-email.ts
+++ pages/api/send-travel-email.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../middleware/database';
 import { getSession } from 'next-auth/react';
 import User from '../../models/user';
@@ -8,19 +8,19 @@ import { sendTravelEmail } from './emails/aws';
 import { UserSchema } from '../../models/user';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-    //const session = await getSession({ req });
-    //if (session?.userType !== 'ORGANIZER') return res.status(403).send('Forbidden');
-    // TODO: add uniqueness validation try catch
-    await dbConnect();
-    switch (req.method) {
+	//const session = await getSession({ req });
+	//if (session?.userType !== 'ORGANIZER') return res.status(403).send('Forbidden');
+	// TODO: add uniqueness validation try catch
+	await dbConnect();
+	switch (req.method) {
 		case 'GET':
 			const logins = await User.find({}).select('id name email applicationStatus');
-			
+
 			return res.status(200).send(logins);
 		case 'POST':
 			const { UserData: userData } = req.body;
 
-			sendTravelEmail(req.body)
+			sendTravelEmail(req.body);
 			return res.status(200).send(`Send travel email to ${req.body.email}`);
 		default:
 			return res.status(405).send('Method not supported brother');
