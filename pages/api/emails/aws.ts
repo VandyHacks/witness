f2984d5b0b9a62727f60c4c2815eeaeac47b8d43
Sneh Patel <sneh@Snehs-MacBook-Pro.local>
2022-09-27 17:16:22 -0500

    Fixing vercel build pls

--- pages/api/emails/aws.ts
+++ pages/api/emails/aws.ts
@@ -8,7 +8,7 @@ import rejected from './templates/rejected';
 import logger from '../logger';
 import travelForm from './templates/travelForm';
 
-const AWS_REGION = process.env.AWS_REGION as string;
+const AWS_REGION = process.env.VH_AWS_REGION as string;
 
 if (AWS_REGION == null) {
 	throw new Error('AWS_REGION not set');
@@ -20,9 +20,9 @@ const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 
 export function sendTravelEmail(user: UserData): void {
 	let email: AWS.SES.SendEmailRequest;
-	email = travelForm(user);
+	email = travelForm(user)
 
-	var sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(email).promise();
+	var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(email).promise();
 	console.log(sendPromise);
 }
 
@@ -62,4 +62,4 @@ export function sendStatusEmail(user: UserData, status: ApplicationStatus): void
 	*/
 }
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { sendStatusEmail, sendTravelEmail };
+export default { sendStatusEmail, sendTravelEmail};
