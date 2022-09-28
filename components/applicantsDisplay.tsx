2ad8e195ab0d623f356b7c2d16208bac15f4125e
Akash Munagala <akash.munagala@gmail.com>
2022-09-27 19:29:37 -0500

    Populate data from application object in modal

--- components/applicantsDisplay.tsx
+++ components/applicantsDisplay.tsx
@@ -4,23 +4,65 @@ import React, { useState } from 'react';
 import { ApplicationData, UserData } from '../types/database';
 import { ExportToCsv } from 'export-to-csv';
 import { CheckCircleOutlined, EyeOutlined } from '@ant-design/icons';
+import { DateTime } from 'luxon';
 
 export interface ApplicantsDisplayProps {
 	hackers: UserData[];
 	applications: ApplicationData[];
 }
 
-const APPLICATION_STATUSES = ['Created', 'Declined', 'Started', 'Submitted', 'Accepted', 'Confirmed', 'Rejected', 'Checked In'];
+const APPLICATION_STATUSES = [
+	'Created',
+	'Declined',
+	'Started',
+	'Submitted',
+	'Accepted',
+	'Confirmed',
+	'Rejected',
+	'Checked In',
+];
 
 const STATUS_COLORS = {
-	'Created': 'magenta',
-	'Declined': 'red',
-	'Started': 'cyan',
-	'Submitted': 'gold',
-	'Accepted': 'success',
-	'Confirmed': 'purple',
-	'Rejected': 'volcano',
+	Created: 'magenta',
+	Declined: 'red',
+	Started: 'cyan',
+	Submitted: 'gold',
+	Accepted: 'success',
+	Confirmed: 'purple',
+	Rejected: 'volcano',
 	'Checked In': 'green',
+};
+
+const APPLICATION_KEY_MAP = {
+	firstName: 'First Name',
+	lastName: 'Last Name',
+	preferredName: 'Preferred Name',
+	gender: 'Gender',
+	dateOfBirth: 'Date of Birth',
+	phoneNumber: 'Phone Number',
+	school: 'School',
+	major: 'Major',
+	graduationYear: 'Graduation Year',
+	address1: 'Address 1',
+	address2: 'Address 2',
+	city: 'City',
+	state: 'State',
+	zip: 'Zip',
+	race: 'Race',
+	dietaryRestrictions: 'Dietary Restrictions',
+	accommodationNeeds: 'Accomodation Needs',
+	firstTime: 'First Time',
+	whyAttend: 'Why attend',
+	techIndustry: 'Tech Industry',
+	techStack: 'Tech Stack',
+	passion: 'Passion',
+	motivation: 'Motivation',
+	shirtSize: 'Shirt Size',
+	applyTravelReimbursement: 'Apply Travel Reimbursement',
+	overnight: 'Overnight',
+	prizeEligibility: 'Prize Eligibility',
+	volunteer: 'Volunteer',
+	mlhComms: 'MLH Communications',
 };
 
 export default function ApplicantsDisplay(props: ApplicantsDisplayProps) {
@@ -81,11 +123,11 @@ export default function ApplicantsDisplay(props: ApplicantsDisplayProps) {
 			title: '',
 			render: (text: any, record: any) => (
 				<>
-					{ record.status === 'Confirmed' && <CheckCircleOutlined /> }&nbsp;&nbsp;
-					{ record.status !== 'Created' && <EyeOutlined onClick={() => openModal(record._id)} /> }
+					{record.status === 'Confirmed' && <CheckCircleOutlined />}&nbsp;&nbsp;
+					{record.status !== 'Created' && <EyeOutlined onClick={() => openModal(record._id)} />}
 				</>
 			),
-		}
+		},
 	];
 
 	const openModal = (id: string) => {
@@ -98,17 +140,42 @@ export default function ApplicantsDisplay(props: ApplicantsDisplayProps) {
 		setIsModalOpen(false);
 	};
 
+	const createSingleApplicantEntry = ([field, response]: [string, string | boolean | string[] | undefined]) => {
+		console.log(field, response);
+		switch (typeof response) {
+			case 'string':
+				const dateTime = DateTime.fromISO(response);
+				response = dateTime.isValid ? dateTime.toLocaleString(DateTime.DATE_MED) : response;
+				response = response === '' ? undefined : response;
+				break;
+			case 'boolean':
+				response = response ? 'Yes' : 'No';
+				break;
+			case 'object':
+				response = response.length === 0 ? 'None' : response.join(', ');
+				break;
+		}
+
+		return (
+			<p key={field}>
+				{(APPLICATION_KEY_MAP as any)[field]}: {response ? response : <i>Not provided</i>}
+			</p>
+		);
+	};
+
 	return (
 		<>
-			<Table style={{width: "95vw"}} dataSource={allApplicantsData} columns={newCols}></Table>
+			<Table style={{ width: '95vw' }} dataSource={allApplicantsData} columns={newCols}></Table>
 			{isModalOpen && (
 				<Modal
 					title="Hacker's Application Form"
 					visible={isModalOpen}
 					onOk={handleCloseModal}
 					onCancel={handleCloseModal}>
-					<p>First Name: {singleApplicant?.firstName ? singleApplicant.firstName : 'Not Provided'}</p>
-					<p>Last name: {singleApplicant?.lastName}</p>
+					{singleApplicant &&
+						Object.entries(singleApplicant)
+							.filter(([field, _]) => field in APPLICATION_KEY_MAP)
+							.map(createSingleApplicantEntry)}
 				</Modal>
 			)}
 		</>
