fc09ae9fcb0bea080f52d3b75153793b904e04da
Akash Munagala <akash.munagala@gmail.com>
2022-10-23 04:16:04 -0500

    Fix typescript issue

--- types/database.ts
+++ types/database.ts
@@ -113,7 +113,7 @@ export interface EventData {
 }
 
 export interface JudgingSessionData {
-	_id: mongoose.Schema.Types.ObjectId;
+	_id?: mongoose.Schema.Types.ObjectId;
 	team: TeamData;
 	judge: UserData;
 	time: String;
