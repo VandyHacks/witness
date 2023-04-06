f8e2295f1bf0b964808c89ed09a4ee7bb000577e
Carol He <carolhe.che.17@gmail.com>
2023-04-06 14:26:41 -0500

    update judge component file names

--- components/judges/judgingForm.tsx
+++ components/judges/judgingForm.tsx
@@ -1,4 +1,4 @@
-import ScoreInput from './scoreInput';
+import ScoreInput from './ScoreInput';
 import { Form, Input, Button } from 'antd';
 import { JudgingFormFields } from '../../types/client';
 import { useEffect } from 'react';
