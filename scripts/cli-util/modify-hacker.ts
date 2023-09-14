0e3e80eb3e343faf5362d0940c127d688e792fb6
Zi Nean Teoh <zinean00@gmail.com>
2023-09-13 23:32:29 -0500

    finish implement changeStatus for hacker doc

--- scripts/cli-util/modify-hacker.ts
+++ scripts/cli-util/modify-hacker.ts
@@ -68,44 +68,51 @@ export const handleModifyHacker = async () => {
 
 const changeStatus = async (hacker: UserData) => {
 	const oldStatus: ApplicationStatus = hacker.applicationStatus;
+	const oldStatusString = getApplicationStatusString(oldStatus);
 
-	// console.log(`Current status: ${Object.values(ApplicationStatus)[oldStatus]}`);
+	console.log(`Current status: ${oldStatus} (${oldStatusString})`);
 
-	const newStatus = await select({
+	const newStatus: ApplicationStatus = await select({
 		message: 'Select new status',
 		choices: [
 			{
 				name: 'Change application status to CREATED',
-				value: 'created',
+				value: ApplicationStatus.CREATED,
 			},
 			{
 				name: 'Change application status to DECLINED',
-				value: 'declined',
+				value: ApplicationStatus.DECLINED,
+			},
+			{
+				name: 'Change application status to STARTED',
+				value: ApplicationStatus.STARTED,
 			},
 			{
 				name: 'Change application status to SUBMITTED',
-				value: 'submitted',
+				value: ApplicationStatus.SUBMITTED,
 			},
 			{
 				name: 'Change application status to ACCEPTED',
-				value: 'accepted',
+				value: ApplicationStatus.ACCEPTED,
 			},
 			{
 				name: 'Change application status to CONFIRMED',
-				value: 'confirmed',
+				value: ApplicationStatus.CONFIRMED,
 			},
 			{
 				name: 'Change application status to REJECTED',
-				value: 'rejected',
+				value: ApplicationStatus.REJECTED,
 			},
 			{
 				name: 'Change application status to CHECKED_IN',
-				value: 'checked-in',
+				value: ApplicationStatus.CHECKED_IN,
 			},
 		],
 	});
 
-	// TODO:
+	await User.updateOne({ email: hacker.email }, { applicationStatus: newStatus });
+
+	console.log(`Changed application status from ${oldStatus} to ${newStatus}`);
 };
 
 const deleteApplication = async () => {};
@@ -115,3 +122,24 @@ const joinTeam = async () => {};
 const leaveTeam = async () => {};
 
 const nfcCheckIn = async () => {};
+
+const getApplicationStatusString = (status: ApplicationStatus): string => {
+	switch (status) {
+		case ApplicationStatus.CREATED:
+			return 'CREATED';
+		case ApplicationStatus.DECLINED:
+			return 'DECLINED';
+		case ApplicationStatus.STARTED:
+			return 'STARTED';
+		case ApplicationStatus.SUBMITTED:
+			return 'SUBMITTED';
+		case ApplicationStatus.ACCEPTED:
+			return 'ACCEPTED';
+		case ApplicationStatus.CONFIRMED:
+			return 'CONFIRMED';
+		case ApplicationStatus.REJECTED:
+			return 'REJECTED';
+		case ApplicationStatus.CHECKED_IN:
+			return 'CHECKED_IN';
+	}
+};
