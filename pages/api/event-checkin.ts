f63934eb418a2743f3f8bb14828a81039d56afad
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-09-16 15:40:09 -0500

    Finish updating NFC points while checking in user

--- pages/api/event-checkin.ts
+++ pages/api/event-checkin.ts
@@ -10,15 +10,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	await dbConnect();
 	switch (req.method) {
 		case 'POST':
-			const { nfcId, eventId } = req.body;
+			const { nfcId, eventId, nfcPoints } = req.body;
+			console.log('NFC Point: ', nfcPoints);
 			if (!nfcId || !eventId) return res.status(400).send('An NFC id and event id are needed');
 
-			const alreadyCheckedIn = await User.findOne({ nfcId, eventsAttended: eventId });
-			if (alreadyCheckedIn) return res.status(451).send('User already checked in');
+			try {
+				const alreadyCheckedIn = await User.findOne({ nfcId, eventsAttended: eventId });
+				if (alreadyCheckedIn) return res.status(451).send('User already checked in');
 
-			const result = await User.updateOne({ nfcId }, { $addToSet: { eventsAttended: eventId } });
-			if (result.matchedCount === 0) return res.status(400).send('User not found');
-			return res.status(200).send(`Checked in ${nfcId} for event ${eventId}`);
+				const filter = { nfcId };
+				const update = {
+					$inc: { nfcPoints: nfcPoints }, // Increment nfcPoints
+					$addToSet: { eventsAttended: eventId }, // Add new event to set
+				};
+				const options = { new: true }; // Return the updated document
+				const user = await User.findOneAndUpdate(filter, update, options);
+				return res.status(200).send(`Checked in ${nfcId} for event ${eventId}`);
+			} catch (error) {
+				console.log(error);
+				return res.status(500).json({ error: 'An error occurred while updating the user' });
+			}
+
 		default:
 			return res.status(405).send('Method not supported brother');
 	}
