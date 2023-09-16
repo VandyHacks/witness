147ba76cb09aea78d3951a84c3c9d7df4ab19acc
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-09-15 20:40:48 -0500

    Styled

--- components/hacker/HackerDash.tsx
+++ components/hacker/HackerDash.tsx
@@ -713,37 +713,35 @@ export default function HackerDash({ userApplicationStatus, setUserApplicationSt
 						(user.applicationStatus === ApplicationStatus.CONFIRMED ||
 							user.applicationStatus === ApplicationStatus.CHECKED_IN) && (
 							<>
-								<>
-									{/* Hacking start code */}
-									<div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '10px' }}>
-										<Button size="small" type="default" onClick={() => signOut()}>
-											Sign out
-										</Button>
-										<div style={{ paddingLeft: '10px', color: 'white' }}>
-											Signed in as {session?.user?.email}
-										</div>
+								{/* Hacking start code */}
+								<div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '10px' }}>
+									<Button size="small" type="default" onClick={() => signOut()}>
+										Sign out
+									</Button>
+									<div style={{ paddingLeft: '10px', color: 'white' }}>
+										Signed in as {session?.user?.email}
 									</div>
-									{!teamData && <TeamSetup />}
-									{teamData && (
-										<div style={{ width: '60vw', margin: 'auto' }}>
-											<Content style={{ width: '60vw', margin: 'auto' }}>
-												<Table
-													locale={{
-														emptyText: (
-															<div style={{ paddingTop: '50px', paddingBottom: '50px' }}>
-																<h3>Stay tuned! You will see your schedule soon!</h3>
-															</div>
-														),
-													}}
-													columns={judgingSessionColumns}
-													dataSource={judgingSessionData}
-												/>
-												<Divider />
-											</Content>
-											<TeamManager profile={teamData} />
-										</div>
-									)}
-								</>
+								</div>
+								{!teamData && <TeamSetup />}
+								{teamData && (
+									<div style={{ width: '60vw', margin: 'auto' }}>
+										<Content style={{ width: '60vw', margin: 'auto' }}>
+											<Table
+												locale={{
+													emptyText: (
+														<div style={{ paddingTop: '50px', paddingBottom: '50px' }}>
+															<h3>Stay tuned! You will see your schedule soon!</h3>
+														</div>
+													),
+												}}
+												columns={judgingSessionColumns}
+												dataSource={judgingSessionData}
+											/>
+											<Divider />
+										</Content>
+										<TeamManager profile={teamData} />
+									</div>
+								)}
 							</>
 						)}
 					{!hackthonStarted &&
