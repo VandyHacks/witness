a74e218ba18a2ae7792c3eb4feffcb14caba6511
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-09-23 15:55:52 -0500

    Isolate Registration Logo out as a component and removed it from hacker portal

---
+++ components/hacker/RegistrationLogo.tsx
@@ -0,0 +1,17 @@
+import { Form } from 'antd';
+import React from 'react';
+
+import styles from '../../styles/Form.module.css';
+
+const RegistrationLogo = () => {
+	return (
+		<>
+			<Form.Item className={styles.TitleLogo}> </Form.Item>
+			<div className={styles.TitleContainer}>
+				<div className={styles.Title}>VandyHacks X Registration</div>
+			</div>
+		</>
+	);
+};
+
+export default RegistrationLogo;
