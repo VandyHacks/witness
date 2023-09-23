05c78cecab077035eeb407ddb1ac0e0eeeb8d958
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-09-23 16:55:54 -0500

    Finish drawing the layout

--- components/hacker/hacking-start/Header.tsx
+++ components/hacker/hacking-start/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from '../../../styles/hacker/hacking-start.module.css';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<>
+			<div className={styles.HeaderBox}>
+				<div className={styles.HeaderTopBox}>
+					<span className={styles.Logo}></span>
+					<span className={styles.SignOut}></span>
+				</div>
+				<div className={styles.HeaderMiddleBox}>
+					<span className={styles.UserName}></span>
+					<span className={styles.CurEvent}></span>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Header;
