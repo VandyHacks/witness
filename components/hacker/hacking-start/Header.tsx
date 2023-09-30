d25eb3806fb21274cf26fcb42d2e84578e821b3e
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-09-30 15:19:13 -0500

    Add logo

--- components/hacker/hacking-start/Header.tsx
+++ components/hacker/hacking-start/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from '../../../styles/hacker/hacking-start.module.css';
+import Image from 'next/image';
+import VHLogo from '../../../public/vh-logo.png';
 
 const Header = () => {
 	return (
 		<>
 			<div className={styles.HeaderBox}>
 				<div className={styles.HeaderTopBox}>
-					<span className={styles.Logo}></span>
+					<Image alt="" width={175} height={175} src={VHLogo} className={styles.Logo}></Image>
 					<span className={styles.SignOut}></span>
 				</div>
 				<div className={styles.HeaderMiddleBox}>
