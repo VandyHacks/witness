794599ff5493ec79431beb60b07fbf41a35ea5aa
Zi Nean Teoh <zinean00@gmail.com>
2023-10-03 19:14:58 -0500

    fix codescanning error

--- pages/api/theme.ts
+++ pages/api/theme.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
 				return res.status(200).send(newUser);
 			} catch (err) {
-				return res.status(500).send(err);
+				return res.status(500).send("Couldn't update user settings");
 			}
 		default:
 			return res.status(405).send('Method not supported brother');
