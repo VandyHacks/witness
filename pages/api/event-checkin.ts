949a41be41a1ca94ee28aea522e4a318906f0ac8
Zi Nean Teoh <zinean00@gmail.com>
2023-10-06 14:38:38 -0500

    fix codeql vuln and refactor api

--- pages/api/event-checkin.ts
+++ pages/api/event-checkin.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../middleware/database';
 import { getSession } from 'next-auth/react';
 import User from '../../models/user';
+import Event from '../../models/event';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
 	const session = await getSession({ req });
@@ -15,17 +16,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 			if (!nfcId || !eventId) return res.status(400).send('An NFC id and event id are needed');
 
 			try {
-				const alreadyCheckedIn = await User.findOne({ nfcId, eventsAttended: eventId });
-				if (alreadyCheckedIn) return res.status(451).send('User already checked in');
+				// ensure that user exists
+				const user = await User.findOne({ nfcId: { $eq: nfcId } });
+				if (!user) return res.status(404).send('User not found');
 
-				const filter = { nfcId };
-				const update = {
-					$inc: { nfcPoints: nfcPoints }, // Increment nfcPoints
-					$addToSet: { eventsAttended: eventId }, // Add new event to set
-				};
-				const options = { new: true }; // Return the updated document
-				const user = await User.findOneAndUpdate(filter, update, options);
-				return res.status(200).send(`Checked in ${nfcId} for event ${eventId}`);
+				// ensure that event exists
+				const event = await Event.findOne({ _id: { $eq: eventId } });
+				if (!event) return res.status(404).send('Event not found');
+
+				// ensure that user is not already checked in
+				const alreadyCheckedIn = user.eventsAttended.includes(eventId);
+				if (alreadyCheckedIn) return res.status(451).send('User already checked in to the event');
+
+				// check in the user
+				user.nfcPoints += nfcPoints;
+				user.eventsAttended.push(eventId);
+
+				// save the user
+				const updatedUser = await user.save();
+
+				return res
+					.status(200)
+					.send(
+						`Checked in ${updatedUser?._id} for event ${eventId} and updated their nfcPoints to ${updatedUser?.nfcPoints}`
+					);
 			} catch (error) {
 				console.log(error);
 				return res.status(500).json({ error: 'An error occurred while updating the user' });
