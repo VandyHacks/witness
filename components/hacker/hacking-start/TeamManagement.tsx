415861fd31f95103d991a0fdd05e086ac03f3193
Jiashu Huang <jiashu.huang@vanderbilt.edu>
2023-10-26 15:32:08 -0500

    Finish rename team logic

--- components/hacker/hacking-start/TeamManagement.tsx
+++ components/hacker/hacking-start/TeamManagement.tsx
@@ -6,14 +6,23 @@ import useSWR, { mutate } from 'swr';
 import { handleSubmitFailure, handleSubmitSuccess } from '../../../lib/helpers';
 
 const TeamManagement = () => {
+	// Team to join
 	const [teamName, setTeamName] = useState<string | undefined>(undefined);
 	const [joinCode, setTeamCode] = useState<string | undefined>(undefined);
+	const [showRenameTeam, setShowRenameTeam] = useState<boolean>(false);
+	const [showChangeDevpost, setShowChangeDevpost] = useState<boolean>(false);
+	// New team name to update
+	const [newTeamName, setNewTeamName] = useState<string | undefined>('');
+	const [newDevPost, setNewDevPost] = useState<string>('');
 
-	const { data: teamData, error: teamError } = useSWR('/api/team', async url => {
+	const {
+		data: teamData,
+		error: teamError,
+		mutate,
+	} = useSWR('/api/team', async url => {
 		const res = await fetch(url, { method: 'GET' });
 		if (!res.ok) return;
 		const { members, ...rest } = await res.json();
-
 		return { members: members.map((member: any) => member.name), ...rest } as TeamProfile;
 	});
 
@@ -27,8 +36,33 @@ const TeamManagement = () => {
 		const res = await fetch(endpoint, { method, headers, body });
 
 		if (res.ok) {
-			mutate(endpoint);
+			// mutate(endpoint);
 			handleSubmitSuccess(`Successfully ${action === 'CREATE' ? 'created' : 'joined'} a team!`);
+		} else {
+			handleSubmitFailure(await res.text());
+		}
+	};
+
+	const handleUpdateTeam = async () => {
+		if (!newTeamName) {
+			// Empty team name
+			handleSubmitFailure('Team name cannot be empty!');
+			setShowRenameTeam(false);
+			return;
+		}
+		const endpoint = '/api/team-update';
+		const method = 'PATCH';
+		const headers = { 'Content-Type': 'application/json' };
+		const body = JSON.stringify({ teamName: newTeamName });
+
+		const res = await fetch(endpoint, { method, headers, body });
+
+		if (res.ok) {
+			// mutate(endpoint);
+			setShowRenameTeam(false);
+			// Update the teamData
+			mutate();
+			handleSubmitSuccess(`Successfully Changed Team Name!`);
 		} else {
 			handleSubmitFailure(await res.text());
 		}
@@ -68,15 +102,40 @@ const TeamManagement = () => {
 			)}
 			{teamData && (
 				<>
-					<div>Team Name: {teamData.name}</div>
+					{!showRenameTeam && (
+						<>
+							<div>Team Name: {teamData.name}</div>
+						</>
+					)}
+
+					{showRenameTeam && (
+						<>
+							<div className={styles.TeamNameInputContainer}>
+								<span>Team Name:</span>
+								<Input
+									onPressEnter={handleUpdateTeam}
+									onChange={event => setNewTeamName(event.target.value)}
+									className={styles.TeamNameInput}
+									placeholder="New Team Name"
+									defaultValue={teamData.name}
+								/>
+							</div>
+						</>
+					)}
+
 					<div>Members: {teamData.members}</div>
 					<div>Join Code: {teamData.joinCode}</div>
 					<div>Devpost: {teamData.devpost}</div>
+
 					<div className={styles.TeamButtonContainer}>
 						{/* TODO: change team name */}
-						<button className={styles.TeamButton}>Rename Team</button>
+						<button className={styles.TeamButton} onClick={() => setShowRenameTeam(true)}>
+							Rename Team
+						</button>
 						{/* TODO: change devpost */}
-						<button className={styles.TeamButton}>Change Devpost</button>
+						<button className={styles.TeamButton} onClick={() => setShowChangeDevpost(true)}>
+							Change Devpost
+						</button>
 						{/* TODO: leave team button */}
 						<button className={styles.TeamButton}>Leave the Team</button>
 					</div>
