f396fb563335d8d2b6d363137c19b4a252914658
Zi Nean Teoh <58854510+zineanteoh@users.noreply.github.com>
2023-10-27 00:07:33 -0500

    Hacker Portal Team Management (#406)
    * checkpoint for team management
    
    * clean things up
    
    * checkpoint for team management
    
    * handle no webhook env
    
    * dont require devpost
    
    * delete old components
    
    * delete old api endpoint
    
    * add 5 endpoints for team
    
    * connect <TeamManagement/> to backend
    
    * clean up <HackerDash/>
    
    * fix cli
    
    * improve error message
    
    * Add the three buttons
    
    * Finish rename team logic
    
    * Run Prettier
    
    * Style the buttons mobile-responsively
    
    * Add devpost button
    
    * Run Prettier
    
    * Update team name and devpost done
    
    * Run Prettier
    
    * Leave team logic
    
    * Edit the join team error message
    
    * remove package-lock
    
    * clean up code, fix english
    
    * switch to hav consistent button style
    
    * remove unnecessary fragments
    
    * use antd modal. finish(?) <TeamManagement/>
    
    * add description for judging
    
    * handle error devpost empty
    
    * fix mobile
    
    * points are for redeeming prizes
    
    * Parse the members list
    
    * Fix mobile responsiveness
    
    * Run Prettier
    
    * Use flexbox for mobile
    
    * Run Prettier
    
    ---------
    
    Co-authored-by: Jiashu Huang <jiashu.huang@vanderbilt.edu>
    Co-authored-by: JiashuHarryHuang <JiashuHarryHuang@users.noreply.github.com>
    Co-authored-by: zineanteoh <zineanteoh@users.noreply.github.com>

---
+++ components/hacker/hacking-start/TeamManagement.tsx
@@ -0,0 +1,253 @@
+import React, { useState } from 'react';
+import { TeamProfile } from '../../../types/client';
+import styles from '../../../styles/hacker/Table.module.css';
+import { Button, Input, Modal } from 'antd';
+import useSWR, { mutate } from 'swr';
+import { handleSubmitFailure, handleSubmitSuccess } from '../../../lib/helpers';
+
+const isDevpostURL = (input: string): boolean => {
+	if (!input) return false;
+
+	return input.startsWith('https://devpost.com/') || input.startsWith('http://www.devpost.com/');
+};
+
+const TeamManagement = () => {
+	// Team to join
+	const [teamName, setTeamName] = useState<string | undefined>(undefined);
+	const [joinCode, setTeamCode] = useState<string | undefined>(undefined);
+	const [showRenameTeam, setShowRenameTeam] = useState<boolean>(false);
+	const [showChangeDevpost, setShowChangeDevpost] = useState<boolean>(false);
+	// New team name to update
+	const [newTeamName, setNewTeamName] = useState<string | undefined>('');
+	const [newDevPost, setNewDevPost] = useState<string>('');
+	const [showLeaveModal, setShowLeaveModal] = useState<boolean>(false);
+	const [showRenameTeamModal, setShowRenameTeamModal] = useState<boolean>(false);
+
+	const {
+		data: teamData,
+		error: teamError,
+		mutate,
+	} = useSWR('/api/team', async url => {
+		const res = await fetch(url, { method: 'GET' });
+		if (!res.ok) return;
+		const { members, ...rest } = await res.json();
+		return { members: members.map((member: any) => member.name), ...rest } as TeamProfile;
+	});
+
+	// handle create/join team
+	const handleAction = async (action: 'CREATE' | 'JOIN') => {
+		const endpoint = action === 'CREATE' ? '/api/team-create' : '/api/team-join';
+		const method = action === 'CREATE' ? 'POST' : 'PATCH';
+		const headers = { 'Content-Type': 'application/json' };
+		const body = JSON.stringify(action === 'CREATE' ? { teamName } : { joinCode });
+
+		const res = await fetch(endpoint, { method, headers, body });
+
+		if (res.ok) {
+			// mutate(endpoint);
+			handleSubmitSuccess(`Successfully ${action === 'CREATE' ? 'created' : 'joined'} a team!`);
+			window.location.reload();
+		} else {
+			handleSubmitFailure('There was an error. Are you sure the team is not filled up already?');
+		}
+	};
+
+	const handleUpdateTeam = async () => {
+		if (!newTeamName) {
+			// Empty team name
+			handleSubmitFailure('Team name cannot be empty!');
+			setShowRenameTeam(false);
+			return;
+		}
+		const endpoint = '/api/team-update';
+		const method = 'PATCH';
+		const headers = { 'Content-Type': 'application/json' };
+		const body = JSON.stringify({ teamName: newTeamName });
+
+		const res = await fetch(endpoint, { method, headers, body });
+		const message = await res.text();
+
+		if (res.ok) {
+			// mutate(endpoint);
+			setShowRenameTeam(false);
+			// Update the teamData
+			mutate();
+			handleSubmitSuccess(`Successfully Changed Team Name!`);
+
+			// Reset state
+			setNewTeamName('');
+		} else {
+			// handle submit failure the resupose message
+			handleSubmitFailure(message);
+		}
+	};
+
+	const handleUpdateDevpost = async () => {
+		if (!newDevPost) {
+			// Empty team name
+			handleSubmitFailure('Devpost link cannot be empty!');
+			setShowChangeDevpost(false);
+			return;
+		}
+
+		if (!isDevpostURL(newDevPost)) {
+			// Valid Devpost URL
+			handleSubmitFailure(
+				'Devpost link is invalid. Must start with https://devpost.com/ or https://www.devpost.com/'
+			);
+			setShowChangeDevpost(false);
+			return;
+		}
+
+		const endpoint = '/api/team-update';
+		const method = 'PATCH';
+		const headers = { 'Content-Type': 'application/json' };
+		const body = JSON.stringify({ teamName: '', devpost: newDevPost });
+
+		const res = await fetch(endpoint, { method, headers, body });
+		const message = await res.text();
+
+		if (res.ok) {
+			// mutate(endpoint);
+			setShowChangeDevpost(false);
+			// Update the teamData
+			mutate();
+			handleSubmitSuccess(`Successfully Changed Devpost Link!`);
+
+			// Reset state
+			setNewDevPost('');
+		} else {
+			handleSubmitFailure(message);
+		}
+	};
+
+	const handleLeaveTeam = async () => {
+		const endpoint = '/api/team-leave';
+		const method = 'PATCH';
+		const headers = { 'Content-Type': 'application/json' };
+
+		const res = await fetch(endpoint, { method, headers });
+		const message = await res.text();
+
+		if (res.ok) {
+			handleSubmitSuccess(`You have left the team`);
+
+			// Reset state
+			setShowLeaveModal(false);
+
+			window.location.reload();
+		} else {
+			handleSubmitFailure(message);
+		}
+	};
+
+	return (
+		<div className={styles.Container}>
+			Team
+			<div className={styles.Description}>
+				You can create a team or join a team (1 - 4 people per team). Devpost link must be submitted by 12:30 PM
+				on October 29th.
+			</div>
+			{!teamData && (
+				<>
+					<div className={styles.Placeholder}>You are not in a team yet.</div>
+					<div className={styles.TeamActionContainer}>
+						<div>
+							<div>Create a Team</div>
+							<div className={styles.TeamInput}>
+								<Input placeholder="Enter Team Name" onChange={e => setTeamName(e.target.value)} />
+								<Button htmlType="submit" onClick={() => handleAction('CREATE')}>
+									Create Team
+								</Button>
+							</div>
+						</div>
+
+						<div>Or</div>
+
+						<div>
+							<div>Join a Team</div>
+							<div className={styles.TeamInput}>
+								<Input placeholder="Enter Team Code" onChange={e => setTeamCode(e.target.value)} />
+								<Button htmlType="submit" onClick={() => handleAction('JOIN')}>
+									Join Team
+								</Button>
+							</div>
+						</div>
+					</div>
+				</>
+			)}
+			{teamData && (
+				<>
+					<div className={styles.TeamContainer}>
+						<div className={styles.TeamRow}>
+							<div className={styles.TeamRowLabel}>Team Name:</div>
+							<div className={styles.TeamRowValue}>{teamData.name}</div>
+						</div>
+						<div className={styles.TeamRow}>
+							<div className={styles.TeamRowLabel}>Members:</div>
+							<div className={styles.TeamListValue}>
+								{teamData.members.map(member => {
+									return (
+										<div key={member} style={{ marginRight: '10px' }}>
+											{member}
+										</div>
+									);
+								})}
+							</div>
+						</div>
+						<div className={styles.TeamRow}>
+							<div className={styles.TeamRowLabel}>Join Code:</div>
+							<div className={styles.TeamRowValue}>{teamData.joinCode}</div>
+						</div>
+						<div className={styles.TeamRow}>
+							<div className={styles.TeamRowLabel}>Devpost:</div>
+							<div className={styles.TeamRowValue}>{teamData.devpost}</div>
+						</div>
+					</div>
+
+					<div className={styles.TeamButtonContainer}>
+						<Button htmlType="submit" onClick={() => setShowRenameTeam(true)}>
+							Rename Team
+						</Button>
+						<Button htmlType="submit" onClick={() => setShowChangeDevpost(true)}>
+							Add Devpost Link
+						</Button>
+						<Button htmlType="submit" onClick={() => setShowLeaveModal(true)}>
+							Leave the Team
+						</Button>
+					</div>
+
+					<Modal
+						title="Rename Team"
+						open={showRenameTeam}
+						onCancel={() => setShowRenameTeam(false)}
+						onOk={handleUpdateTeam}
+						destroyOnClose={true}>
+						<Input onChange={event => setNewTeamName(event.target.value)} defaultValue={teamData.name} />
+					</Modal>
+
+					<Modal
+						title="Change Devpost Link"
+						open={showChangeDevpost}
+						onCancel={() => setShowChangeDevpost(false)}
+						onOk={handleUpdateDevpost}
+						destroyOnClose={true}>
+						<Input
+							onChange={event => setNewDevPost(event.target.value)}
+							defaultValue={teamData.devpost && teamData.devpost.toString()}
+						/>
+					</Modal>
+
+					<Modal
+						title="Are you sure you want to leave the current team?"
+						open={showLeaveModal}
+						onOk={handleLeaveTeam}
+						onCancel={() => setShowLeaveModal(false)}
+					/>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default TeamManagement;
--- middleware/log.ts
+++ middleware/log.ts
@@ -1,6 +1,8 @@
 const { SLACK_LOGGING_WEBHOOK } = process.env;
 
 export default async function log(user: any, data: any) {
+	if (!SLACK_LOGGING_WEBHOOK) return;
+
 	await fetch(SLACK_LOGGING_WEBHOOK as string, {
 		method: 'POST',
 		body: JSON.stringify({
