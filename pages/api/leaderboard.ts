0f8bb55655a25bda4f2e301a3ca961c5344539ce
Zi Nean Teoh <zinean00@gmail.com>
2023-10-28 13:40:43 -0500

    hot fix to api/leaderboard mongoose cache
    Co-authored-by: Jacob Lurie <jacoblurie29@gmail.com>

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import dbConnect from '../../middleware/database';
 import User from '../../models/user';
+import Team from '../../models/team';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
 	const session = await getSession({ req });
 	if (session?.userType !== 'HACKER') return res.status(403).send('Forbidden');
 
+	Team; // Don't remove or the import will get optimized out and the populate will fail
 	await dbConnect();
 	switch (req.method) {
 		case 'GET':
