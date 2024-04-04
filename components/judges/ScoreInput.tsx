d4292e108d536520c7fa531a133d466a7f9c0f2f
szheng31 <szheng31@users.noreply.github.com>
2024-04-04 19:12:27 +0000

    Run Prettier

--- components/judges/ScoreInput.tsx
+++ components/judges/ScoreInput.tsx
@@ -1,4 +1,4 @@
-import { Slider, Radio , InputNumber, Row, Col } from 'antd';
+import { Slider, Radio, InputNumber, Row, Col } from 'antd';
 import { RadioChangeEvent } from 'antd/lib';
 import { IntegerType } from 'mongodb';
 import React from 'react';
@@ -11,22 +11,33 @@ export default function ScoreInput(props: ScoreInputProps) {
 	const { value, onChange } = props;
 	const [min, max] = [0, 7];
 	const onRadioChange = (e: RadioChangeEvent) => {
-        onChange(e.target.value);
-    };
-	
+		onChange(e.target.value);
+	};
+
 	return (
 		<Row>
 			<Col span={20}>
 				<Radio.Group onChange={onRadioChange} value={value}>
-					<Radio style={{marginRight: '20px'}} value={1}>1</Radio>
-      				<Radio style={{marginRight: '20px'}} value={2}>2</Radio>
-      				<Radio style={{marginRight: '20px'}} value={3}>3</Radio>
-      				<Radio style={{marginRight: '20px'}} value={4}>4</Radio>
-					<Radio style={{marginRight: '20px'}} value={5}>5</Radio>
-      				<Radio style={{marginRight: '20px'}} value={6}>6</Radio>
-      				<Radio value={7}>7</Radio>
+					<Radio style={{ marginRight: '20px' }} value={1}>
+						1
+					</Radio>
+					<Radio style={{ marginRight: '20px' }} value={2}>
+						2
+					</Radio>
+					<Radio style={{ marginRight: '20px' }} value={3}>
+						3
+					</Radio>
+					<Radio style={{ marginRight: '20px' }} value={4}>
+						4
+					</Radio>
+					<Radio style={{ marginRight: '20px' }} value={5}>
+						5
+					</Radio>
+					<Radio style={{ marginRight: '20px' }} value={6}>
+						6
+					</Radio>
+					<Radio value={7}>7</Radio>
 				</Radio.Group>
-				
 			</Col>
 			<Col span={4}>
 				<InputNumber
