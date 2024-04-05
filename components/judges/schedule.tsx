48225ac5457a9bb4d13f1e74886d137d5d0ba0e3
Sobenna <sobenna.p.onwumelu@vanderbilt.edu>
2024-04-05 14:33:02 -0400

    added row clicking in addition to button clicking. only row is a little unintuitive?

--- components/judges/schedule.tsx
+++ components/judges/schedule.tsx
@@ -250,6 +250,10 @@ export function JudgeSchedule({ data, cutoffIndex, handleChange }: ScheduleProps
 		};
 	});
 
+	const handleRowClick = (record: any) => {
+        handleChange(record.teamId);
+    };
+
 	return (
 		<Table
 			locale={{
@@ -279,6 +283,9 @@ export function JudgeSchedule({ data, cutoffIndex, handleChange }: ScheduleProps
 					</Table.Summary.Row>
 				</Table.Summary>
 			)}
+			onRow={(record) => ({
+                onClick: () => handleRowClick(record),
+            })}
 		/>
 	);
 }
