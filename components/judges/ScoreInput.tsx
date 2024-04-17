3d4766281d4e8f25f43e637de3fe04bbae19071c
Stanley Zheng <stanley.zheng00@gmail.com>
2024-04-16 20:16:53 -0400

    change slider to radio buttons in judge (#456)
    * fixed slider and button spacing
    
    * Run Prettier
    
    * changed slider to group of buttons
    
    * Run Prettier
    
    * fixed buttons and input box
    
    * Run Prettier
    
    ---------
    
    Co-authored-by: Stanley Zheng <szheng31@stuy.edu>
    Co-authored-by: szheng31 <szheng31@users.noreply.github.com>
    Co-authored-by: Isaac  Liu <69013063+xnscdev@users.noreply.github.com>

--- components/judges/ScoreInput.tsx
+++ components/judges/ScoreInput.tsx
@@ -1,5 +1,9 @@
-import { Slider, InputNumber, Row, Col } from 'antd';
+import { Slider, Radio, InputNumber, Row, Col } from 'antd';
+import { RadioChangeEvent } from 'antd/lib';
+import { IntegerType } from 'mongodb';
+import styles from '../../styles/Judge.module.css';
 import React from 'react';
+
 interface ScoreInputProps {
 	value: number;
 	onChange: (value: number | null) => void;
@@ -8,10 +12,39 @@ interface ScoreInputProps {
 export default function ScoreInput(props: ScoreInputProps) {
 	const { value, onChange } = props;
 	const [min, max] = [0, 7];
+	const onRadioChange = (e: RadioChangeEvent) => {
+		onChange(e.target.value);
+	};
+
 	return (
 		<Row>
-			<Col span={20}>
-				<Slider min={min} max={max} onChange={onChange} value={value} />
+			<Col flex={1 / 4}>
+				<Radio.Group
+					onChange={onRadioChange}
+					value={value}
+					style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+					<Radio className={styles.CustomRadio} value={1}>
+						1
+					</Radio>
+					<Radio className={styles.CustomRadio} value={2}>
+						2
+					</Radio>
+					<Radio className={styles.CustomRadio} value={3}>
+						3
+					</Radio>
+					<Radio className={styles.CustomRadio} value={4}>
+						4
+					</Radio>
+					<Radio className={styles.CustomRadio} value={5}>
+						5
+					</Radio>
+					<Radio className={styles.CustomRadio} value={6}>
+						6
+					</Radio>
+					<Radio className={styles.CustomRadio} value={7}>
+						7
+					</Radio>
+				</Radio.Group>
 			</Col>
 			<Col span={4}>
 				<InputNumber
